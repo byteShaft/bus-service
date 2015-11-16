bca80f396151c7c719babdd87289034c6d4d44ac
s9iper1 <s9iper1@gmail.com>
2015-11-16 23:52:06 +0500

    initilize the project

---
+++ app/src/main/java/com/byteshaft/busservice/fragments/MapsFragment.java
@@ -0,0 +1,22 @@
+package com.byteshaft.busservice.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.byteshaft.busservice.R;
+
+public class MapsFragment extends Fragment {
+
+    private View mBaseView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBaseView = inflater.inflate(R.layout.maps, container, false);
+
+
+        return mBaseView;
+    }
+}
