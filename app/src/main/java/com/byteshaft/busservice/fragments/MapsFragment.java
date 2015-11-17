9b8a069da0a731be883a861571ee872788ef76f8
Imran <linuxholic@hotmail.com>
2015-11-17 15:40:55 +0500

    Add maps

--- app/src/main/java/com/byteshaft/busservice/fragments/MapsFragment.java
+++ app/src/main/java/com/byteshaft/busservice/fragments/MapsFragment.java
@@ -2,21 +2,49 @@ package com.byteshaft.busservice.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.byteshaft.busservice.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment{
 
-    private View mBaseView;
+    SupportMapFragment myMapFragment;
+    View convertView;
+    FragmentManager fm;
+    GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mBaseView = inflater.inflate(R.layout.maps, container, false);
+        convertView=inflater.inflate(R.layout.maps, null);
+        fm=getChildFragmentManager();
+        myMapFragment=(SupportMapFragment) fm.findFragmentById(R.id.map);
 
+        myMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
 
-        return mBaseView;
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in this! ===>>"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            }
+        });
+
+        return convertView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 }
