9522d210891eb576fac6380e14ed48e54110a851
Imran <linuxholic@hotmail.com>
2016-01-24 13:16:03 +0500

    Add Google Maps, Implement Routes with multiple waypoints

--- app/src/main/java/com/byteshaft/busservice/fragments/MapsFragment.java
+++ app/src/main/java/com/byteshaft/busservice/fragments/MapsFragment.java
@@ -1,8 +1,10 @@
 package com.byteshaft.busservice.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,9 +13,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.byteshaft.busservice.R;
+import com.directions.route.Route;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class MapsFragment extends Fragment {
 
@@ -21,22 +33,72 @@ public class MapsFragment extends Fragment {
     private FragmentManager fm;
     private SupportMapFragment myMapFragment;
     private GoogleMap mMap;
+    private RoutingListener mRoutingListener;
+
+    private LatLng startPoint = new LatLng(30.162193, 71.441587);
+    private LatLng endPoint = new LatLng(30.173939, 71.466715);
+    private LatLng wayPoint1 = new LatLng(30.177717, 71.452137);
+    private LatLng wayPoint2 = new LatLng(30.181112, 71.463681);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         convertView = inflater.inflate(R.layout.maps, null);
         setHasOptionsMenu(true);
         fm = getChildFragmentManager();
-        myMapFragment=(SupportMapFragment) fm.findFragmentById(R.id.map);
+        myMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
 
         myMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
+//                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                LatLng currentDummyLocation = new LatLng(30.162193, 71.441587);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentDummyLocation, 16.0f));
 
-//                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16.0f));
+                mMap.addMarker(new MarkerOptions().position(startPoint)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus)));
+                mMap.addMarker(new MarkerOptions().position(wayPoint1)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_stop)));
+                mMap.addMarker(new MarkerOptions().position(wayPoint2)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_stop)));
+                mMap.addMarker(new MarkerOptions().position(endPoint)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_finish)));
+
+                Routing routing = new Routing.Builder()
+                        .travelMode(Routing.TravelMode.DRIVING)
+                        .withListener(mRoutingListener)
+                        .waypoints(startPoint, wayPoint1, wayPoint2, endPoint)
+                        .build();
+                routing.execute();
             }
         });
+
+        mRoutingListener = new RoutingListener() {
+            @Override
+            public void onRoutingFailure() {
+
+            }
+
+            @Override
+            public void onRoutingStart() {
+
+            }
+
+            @Override
+            public void onRoutingSuccess(PolylineOptions polylineOptions, Route route) {
+                PolylineOptions mPolylineOptions = new PolylineOptions();
+                mPolylineOptions.color(Color.BLUE);
+                mPolylineOptions.width(15);
+                mPolylineOptions.zIndex(102);
+                mPolylineOptions.addAll(polylineOptions.getPoints());
+                mMap.addPolyline(mPolylineOptions);
+            }
+
+            @Override
+            public void onRoutingCancelled() {
+
+            }
+        };
 
         return convertView;
     }
